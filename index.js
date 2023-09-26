7a4d64daf34efe39d95347afbd9b0efd6d92c62a
Sanjay Puri <39893694+sanjaypuri@users.noreply.github.com>
2023-09-26 10:05:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/api/users', (req, res) => {
+  res.send([
+    {
+      id:1,
+      name:"Sanjay",
+      age:56
+    },
+    {
+      id:2,
+      name:"Sanju",
+      age:53
+    },
+    {
+      id:3,
+      name:"Shubam",
+      age:27
+    },
+    {
+      id:4,
+      name:"Shobit",
+      age:23
+    }
+  ]);
+});
+
+app.listen(8080, ()=> {console.log(`Listning on port 8080`)});
